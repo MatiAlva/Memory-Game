6c63906f47606719454539225aa68a8292089255
matias alvarez <MatiAlva>
2022-02-08 22:30:18 -0300

    Falta darle un poco mas al diseño

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImagenes = [
+  {"src": "/img/1.png", matched: false},
+  {"src": "/img/2.png", matched: false},
+  {"src": "/img/3.png", matched: false},
+  {"src": "/img/4.png", matched: false},
+  {"src": "/img/5.png", matched: false},
+  {"src": "/img/6.png", matched: false}
+]
+
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImagenes, ...cardImagenes]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random() }))
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+
+  useEffect(() => {
+    if(choiceOne && choiceTwo){
+      setDisabled(true)
+      if(choiceOne.src === choiceTwo.src){
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceTwo.src) {
+              return {...card, matched:true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }   
+  }, [choiceOne, choiceTwo]);
+
+
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+            <SingleCard 
+              key={card.id} 
+              card={card}
+              handleChoice={handleChoice}
+              flipped={card === choiceOne || card === choiceTwo || card.matched}
+              disabled={disabled}
+            />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
+    </div>
+  );
+}
+
+export default App;
